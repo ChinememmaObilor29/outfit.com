99a39b4ecbf31cd2b52bfb363ff55da7282642d3
aaronteamAAR <rooneychris83@gmail.com>
2023-10-30 17:40:49 +0100

    header done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'sans': ['"Inter"', 'system-ui'],
+			  },
+			  fontWeight: {
+				thin: '100',
+				hairline: '100',
+				extralight: '200',
+				light: '300',
+				normal: '400',
+				medium: '500',
+				semibold: '600',
+				bold: '700',
+				extrabold: '800',
+				black: '900',
+			  },
+		},
 	},
 	plugins: [],
 }
+
+
+
+//  'Open Sans', sans-serif;
